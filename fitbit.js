d88bd2cae39ff1b9305c28a4fc39c222ca867119
jazzij <jazzij@umich.edu>
2017-10-10 17:51:10 -0500

    added another API call to js file
    Now the file includes /ACTIVITY endpoint (summary) and /ACTIVITY/DATE endpoint which gives much more detail.

--- fitbit.js
+++ fitbit.js
@@ -39,17 +39,58 @@ var processResponse = function(response){
 	}
 }
 
-// HANDLE the data from an /ACTIVITY api endpoint
+// HANDLE the data from an /ACTIVITY api endpoint - summary data
 var processActivity = function(activity){
 	document.getElementById('activityHeader').textContent = 'Fitbit - GET Activity';
 	prettyAct = JSON.stringify(activity, null, 2);
 	console.log(prettyAct)
-	var display = document.getElementById('display');
-	display.textContent = prettyAct;
 	
+	//these are the three "elements" of data returned in the activity summary
+	best = JSON.stringify(activity["best"], null, 2);
+	lifetime = JSON.stringify(activity["best"], null, 2);
+	tracker = JSON.stringify(activity["tracker"], null, 2);
+	
+	var display = document.getElementById('display');
+	display.textContent = best;
 }
 
-// Now, actually go forth and FETCH
+//HANDLE data from /Activity/Date api endpoint - get actual step count data
+var processActivityWithDate = function(activity){
+        //get GOALS (default 10K)
+        var goalSteps = activity["goals"]["steps"];
+
+        //get STEPS BY ACTIVITY
+        var activitySteps = {};
+        var actArray = activity["activities"];
+        var len = actArray.length;
+        for ( var i = 0; i < len; i++){
+                var id = actArray[i]["logId"];
+                var steps = actArray[i]["steps"];
+                console.log(id+ ': '+steps);
+                activitySteps[id] = steps;
+        }
+        var activityStepsStr = JSON.stringify(activitySteps, null, 2);
+
+        //get SUMMARY STEPS TODAY
+        var sumSteps = activity["summary"]["steps"];
+
+        //print out the data (buggy, need to figure out how to print new lines in javascript!)
+        var newpara = document.createElement("p");
+        var br = document.createElement("br");
+        newpara.append( 'Goal Steps:' +goalSteps);
+        newpara.append(br);
+        newpara.append( 'Steps by Activity: ' + activityStepsStr);
+        newpara.append(br);
+        newpara.append( 'Summary (total) Steps Today: '+ sumSteps);
+
+        var nextHeader = document.getElementById("otherHeader");
+        document.body.insertBefore(newpara, nextHeader);
+
+//      console.log(JSON.stringify(activity, null, 2));
+}
+
+
+// Now, actually go forth and FETCH Activity Summary data
 var activityURL = 'https://api.fitbit.com/1/user/-/activities.json';
 fetch( activityURL, 
 	{
@@ -62,3 +103,18 @@ fetch( activityURL,
 	).then(processResponse)
 	.then(processActivity)
 	.catch(function(error){ console.log(error);});
+
+//go forth and fetch Daily Activity Data (much more detail)
+var date = '2017-10-10';
+var activityWithDateUrl = 'https://api.fitbit.com/1/user/-/activities/date/'+date+'.json';
+fetch( activityWithDateUrl,
+        {
+                headers: new Headers({
+                        'Authorization': 'Bearer ' + fbTok
+                }),
+                mode: 'cors',
+                method: 'GET'
+        }
+        ).then(processResponse)
+        .then(processActivityWithDate)
+        .catch(function(error){ console.log(error);});
