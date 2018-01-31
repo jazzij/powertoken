8d1768f06486b6ed941f1da6ad2080071a706233
Abigail Franz <a2zfranz@gmail.com>
2018-01-31 15:27:09 -0600

    Customized the format of the logs

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,16 @@
-import logging
+import logging, datetime
 from pythonjsonlogger import jsonlogger
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+	def add_fields(self, log_record, record, message_dict):
+		super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+		if not log_record.get("timestamp"):
+			now = datetime.utcnow().strftime("%Y-%02m-%02dT%02H:%02M:%02S.%f")
+			log_record["timestamp"] = now
+		if log_record.get("level"):
+			log_record["level"] = log_record["level"].upper()
+		else:
+			log_record["level"] = record.levelname
 
 # System logger
 systemLogFormat = '%(asctime)-15s: %(message)s'
@@ -8,7 +19,7 @@ systemLogger = logging.getLogger("powertoken.system")
 systemLogger.setLevel(logging.INFO)
 systemLoggerFileHandler = logging.FileHandler(systemLogPath)
 systemLoggerFileHandler.setLevel(logging.INFO)
-systemLoggerFileHandler.setFormatter(jsonlogger.JsonFormatter())
+systemLoggerFileHandler.setFormatter(CustomJsonFormatter('(timestamp) (level) (name) (message)'))
 systemLogger.addHandler(systemLoggerFileHandler)
 		
 # Output logger
@@ -18,5 +29,5 @@ outputLogger = logging.getLogger("powertoken.output")
 outputLogger.setLevel(logging.INFO)
 outputLoggerFileHandler = logging.FileHandler(outputLogPath)
 outputLoggerFileHandler.setLevel(logging.INFO)
-outputLoggerFileHandler.setFormatter(jsonlogger.JsonFormatter())
+outputLoggerFileHandler.setFormatter(CustomJsonFormatter('(timestamp) (level) (name) (message)'))
 outputLogger.addHandler(systemLoggerFileHandler)
