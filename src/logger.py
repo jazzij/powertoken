d37fe2cd0317d79fa6db5b4987a99fbcdbbc6d86
Abigail Franz <a2zfranz@gmail.com>
2018-01-31 14:36:25 -0600

    Fixed a little quotation mistake.

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 
 # System logger
 systemLogFormat = '%(asctime)-15s: %(message)s'
-systemLogPath = format("logs/%s_system.log" % (a2zfranz,))
+systemLogPath = format("logs/%s_system.log" % ("a2zfranz",))
 systemLogger = logging.getLogger("powertoken.system")
 systemLogger.setLevel(logging.INFO)
 systemLoggerFileHandler = logging.FileHandler(self.systemLogPath)
@@ -12,7 +12,7 @@ systemLogger.addHandler(systemLoggerFileHandler)
 		
 # Output logger
 outputLogFormat = '%(asctime)s, %(message)s,'
-outputLogPath = format("logs/%s_output.log" % (a2zfranz,))
+outputLogPath = format("logs/%s_output.log" % ("a2zfranz",))
 outputLogger = logging.getLogger("powertoken.output")
 outputLogger.setLevel(logging.INFO)
 outputLoggerFileHandler = logging.FileHandler(self.systemLogPath)
