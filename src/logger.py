1e6c3f4bcab4a9f47139ea82ebf3a21ae552929b
Abigail Franz <a2zfranz@gmail.com>
2018-01-31 15:17:08 -0600

    Oops, had both my logs pointing to the same path.

--- src/logger.py
+++ src/logger.py
@@ -16,7 +16,7 @@ outputLogFormat = '%(asctime)s, %(message)s,'
 outputLogPath = format("logs/%s_output.log" % ("a2zfranz",))
 outputLogger = logging.getLogger("powertoken.output")
 outputLogger.setLevel(logging.INFO)
-outputLoggerFileHandler = logging.FileHandler(systemLogPath)
+outputLoggerFileHandler = logging.FileHandler(outputLogPath)
 outputLoggerFileHandler.setLevel(logging.INFO)
 outputLoggerFileHandler.setFormatter(jsonlogger.JsonFormatter())
 outputLogger.addHandler(systemLoggerFileHandler)
