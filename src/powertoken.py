b925015e6342abbb6515645bbbf6cf5f3191abae
Abigail Franz <a2zfranz@gmail.com>
2018-01-31 15:52:41 -0600

    Fixed datetime error

--- src/powertoken.py
+++ src/powertoken.py
@@ -144,7 +144,7 @@ class PowerToken:
 					"username": username,
 					"wcProgress": wcProgress,
 					"fbStepCount": fbStepCount,
-					"timestamp": datetime.utcnow().strftime("%Y-%02m-%02dT%02H:%02M:%02S")
+					"timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 				}
 				self._log.insert(logEntry)
 
