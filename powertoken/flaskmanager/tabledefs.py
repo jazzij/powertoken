410636dfd0e59687a6be35a43c6b67d9de1f8258
Abigail Franz <a2zfranz@gmail.com>
2018-02-14 19:53:54 -0600

    Fixed absolute path to the database

--- powertoken/flaskmanager/tabledefs.py
+++ powertoken/flaskmanager/tabledefs.py
@@ -4,23 +4,23 @@ from sqlalchemy import Column, DateTime, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-engine = create_engine('sqlite:///export/scratch/ptdata/pt.db', echo=True)
+engine = create_engine('sqlite:////export/scratch/ptdata/pt.db', echo=True)
 Base = declarative_base()
 
 class PtUser(Base):
-    __tablename__ = "users"
- 
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
+   	__tablename__ = "users"
+
+	id = Column(Integer, primary_key=True)
+	username = Column(String)
 	registered_on = Column(DateTime)
-    goal_period = Column(String)
+	goal_period = Column(String)
 	wc_id = Column(String)
 	wc_token = Column(String)
 	fb_token = Column(String)
 
-    def __init__(self, username):
-        self.username = username
-        self.registered_on = datetime.now()
+	def __init__(self, username):
+		self.username = username
+		self.registered_on = datetime.now()
 
 class PtLog(Base):
 	__tablename__ = "logs"
