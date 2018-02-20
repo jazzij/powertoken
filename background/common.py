419158b6db46b6463e8ebf3503fd35e717801750
Abigail Franz <franz322@umn.edu>
2018-02-20 12:13:33 -0600

    Fixed errors in common.py

--- background/common.py
+++ background/common.py
@@ -1,10 +1,11 @@
 """
-module common
-Functions common to both the weconnect and powertoken models.
-Created by Abigail Franz on 2/16/2018
+module common\n
+Functions common to both the weconnect and fitbit modules.\n
+Created by Abigail Franz on 2/16/2018\n
+Last modified by Abigail Franz on 2/20/2018
 """
 
-import logging
+import logging, requests
 
 error_logger = logging.getLogger("ptErrorLogger")
 error_logger.setLevel(logging.ERROR)
@@ -15,7 +16,7 @@ def is_valid(response):
 	"""
 	if response.status_code >= 300:
 		error_logger.error(format(" Request could not be completed. Error: %d %s" 
-				% (response.status_code, response.text())))
+				% (response.status_code, response.text))
 		return False
 	else:
 		return True
