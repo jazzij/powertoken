6d9b10dab1bc2b68dfa90dab6f5ceaa3a05f2638
Kesiel <a2zfranz@gmail.com>
2018-03-23 11:19:50 -0500

    Trying to fix problem detecting whether it's a GET or a POST request.

--- app/routes.py
+++ app/routes.py
@@ -36,75 +36,79 @@ def user_login():
 	form = UserLoginForm()
 
 	# POST: processes the PowerToken login form
-	if form.validate_on_submit():
-		print("user_login form submitted.")
-		username = form.username.data
-		session["username"] = username
-		user = User.query.filter_by(username=username).first()
+	if request.method == "POST":
+		if form.validate_on_submit():
+			print("user_login form submitted.")
+			username = form.username.data
+			session["username"] = username
+			user = User.query.filter_by(username=username).first()
 
-		# If the user has not been added to the database, adds the user to the
-		# database
-		if user is None:
-			print("User {} isn't in the db yet. Adding to db".format(username))
-			user = User(username=username)
-			db.session.add(user)
-			db.session.commit()
+			# If the user has not been added to the database, adds the user to the
+			# database
+			if user is None:
+				print("User {} isn't in the db yet. Adding to db".format(username))
+				user = User(username=username)
+				db.session.add(user)
+				db.session.commit()
 
-			# Redirects to the WC login
-			print("Redirecting to WC login.")
-			return redirect(url_for("user_wc_login"))
-		
-		# If the user exists in the database, but the WEconnect (or Fitbit)
-		# info isn't filled out, redirects to WEconnect login.
-		if user.wc_id is None or user.wc_token is None or user.fb_token is None:
-			print("Not all the info is filled out for {}. Redirecting to WC login.".format(user))
-			return redirect(url_for("user_wc_login"))
-		
-		# If the user exists in the database, and the WEconnect and Fitbit info
-		# is already filled out, skips the login process.
-		session["authenticated"] = True
-		print("All info is filled out for {}. Redirecting to home.".format(user))
-		return redirect(url_for("user_home"))
+				# Redirects to the WC login
+				print("Redirecting to WC login.")
+				return redirect(url_for("user_wc_login"))
+			
+			# If the user exists in the database, but the WEconnect (or Fitbit)
+			# info isn't filled out, redirects to WEconnect login.
+			if user.wc_id is None or user.wc_token is None or user.fb_token is None:
+				print("Not all the info is filled out for {}. Redirecting to WC login.".format(user))
+				return redirect(url_for("user_wc_login"))
+			
+			# If the user exists in the database, and the WEconnect and Fitbit info
+			# is already filled out, skips the login process.
+			session["authenticated"] = True
+			print("All info is filled out for {}. Redirecting to home.".format(user))
+			return redirect(url_for("user_home"))
 
-	# GET: renders the PowerToken login page
-	print("Received GET request for user_login page.")
-	return render_template("user_login.html", form=form, errors=[request.args.get("error")])
+	elif request.method == "GET":
+		# GET: renders the PowerToken login page
+		print("Received GET request for user_login page.")
+		return render_template("user_login.html", form=form, errors=[request.args.get("error")])
 
 @app.route("/user_wc_login", methods=["GET", "POST"])
 def user_wc_login():
 	form = UserWcLoginForm()
 
 	# If submitting the form (POST)
-	if form.validate_on_submit():
-		print("wc_login form submitted.")
-		if not "username" in session:
-			print("'username' not in session.")
-			return redirect(url_for("user_login", error="Invalid user"))
-		user = User.query.filter_by(username=session.get("username")).first()
-		if user is None:
-			print("User with username {} doesn't exist. Redirecting to user_login.".format(session.get("username")))
-			return redirect(url_for("user_login"), error="Invalid user")
+	if request.method == "POST":
+		if form.validate_on_submit():
+			print("wc_login form submitted.")
+			if not "username" in session:
+				print("'username' not in session.")
+				return redirect(url_for("user_login", error="Invalid user"))
+			user = User.query.filter_by(username=session.get("username")).first()
+			if user is None:
+				print("User with username {} doesn't exist. Redirecting to user_login.".format(session.get("username")))
+				return redirect(url_for("user_login"), error="Invalid user")
 
-		# Gets WEconnect info and adds it to the database
-		email = form.email.data
-		password = form.password.data
-		result = login_to_wc(email, password)
-		if not result:
-			print("Incorrect WC email or password. Reloading wc_login page.")
-			errors = ["Incorrect email or password"]
-			return render_template("user_wc_login.html", form=form, errors=errors)
-		user.wc_id = result[0]
-		user.wc_token = result[1]
-		print("Updating {} with wc_id = {} and wc_token = {}".format(user, user.wc_id, user.wc_token))
-		db.session.commit()
+			# Gets WEconnect info and adds it to the database
+			email = form.email.data
+			password = form.password.data
+			result = login_to_wc(email, password)
+			if not result:
+				print("Incorrect WC email or password. Reloading wc_login page.")
+				errors = ["Incorrect email or password"]
+				return render_template("user_wc_login.html", form=form, errors=errors)
+			user.wc_id = result[0]
+			user.wc_token = result[1]
+			print("Updating {} with wc_id = {} and wc_token = {}".format(user, user.wc_id, user.wc_token))
+			db.session.commit()
 
-		# Redirects to the Fitbit login
-		print("Redirecting to fb_login.")
-		return redirect(url_for("user_fb_login"))
+			# Redirects to the Fitbit login
+			print("Redirecting to fb_login.")
+			return redirect(url_for("user_fb_login"))
 
 	# If not submitting the form (GET)
-	print("Received GET request for wc_login page.")
-	return render_template("user_wc_login.html", form=form)
+	elif request.method == "GET":
+		print("Received GET request for wc_login page.")
+		return render_template("user_wc_login.html", form=form)
 
 @app.route("/user_fb_login", methods=["GET", "POST"])
 def user_fb_login():
