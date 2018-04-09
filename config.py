330402ed6f6f5be976ce5c50a3ea188a0387b3ef
Kesiel <a2zfranz@gmail.com>
2018-04-09 15:44:14 -0500

    Configured the email error logging.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """
 Contains the configuration class for the Flask app.\n
-Created by Abigail Franz on 3/12/2018\n
-Last modified by Abigail Franz on 3/25/2018
+Created by Abigail Franz on 3/12/2018.\n
+Last modified by Abigail Franz on 4/9/2018.
 """
 
 import os
@@ -9,13 +9,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Retrieve the CSRF token from a file on the server. Make sure this file isn't
-# world-readable!
+# world-readable! You can generate a good token with os.urandom(12) and then
+# copy-paste it into the file called "csrf".
 skey = ""
 with open("csrf") as fp:
 	skey = fp.read().strip()
 
-# TODO: Configure the mail server and add administrator email addresses in the
-# ADMINS field.
+# TODO: Add administrator email addresses to the ADMINS field.
 class Config(object):
 	"""
 	Name-value pairs for the Flask app configuration.
@@ -26,7 +26,7 @@ class Config(object):
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = os.environ.get("MAIL_SERVER")
 	MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
-	MAIL_USE_TLS = 1
+	MAIL_USE_TLS = int(os.environ.get("MAIL_USE_TLS"))
 	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 	ADMINS = ["franz322@umn.edu"]
