09328e59c81d84d10e1597241cebe56413c95e4f
Abigail Franz <a2zfranz@gmail.com>
2018-04-16 17:53:03 -0500

    Changed fade.py

--- fade.py
+++ fade.py
@@ -15,26 +15,37 @@ def get_days_progress(day):
 	today_score = 0
 	for act in day.days_activities:
 		today_score += (act.completed * act.activity.weight)
+	print("today_score: {}".format(today_score))
 
 	day_1_ago = Day.query.filter_by(date=(day.date - timedelta(1))).first()
 	day_1_ago_score = 0
 	for act in day_1_ago.days_activities:
 		day_1_ago_score += int(act.completed * (act.activity.weight / 2))
+	print("day_1_ago_score: {}".format(day_1_ago_score))
 
 	day_2_ago = Day.query.filter_by(date=(day.date - timedelta(2))).first()
 	day_2_ago_score = 0
 	for act in day_2_ago.days_activities:
 		day_2_ago_score += int(act.completed * (act.activity.weight / 3))
+	print("day_2_ago_score: {}".format(day_2_ago_score))
 
 	day_3_ago = Day.query.filter_by(date=(day.date - timedelta(3))).first()
 	day_3_ago_score = 0
 	for act in day_3_ago.days_activities:
 		day_3_ago_score += int(act.completed * (act.activity.weight / 4))
+	print("day_3_ago_score: {}".format(day_3_ago_score))
 
 	day_4_ago = Day.query.filter_by(date=(day.date - timedelta(4))).first()
 	day_4_ago_score = 0
 	for act in day_4_ago.days_activities:
 		day_4_ago_score += int(act.completed * (act.activity.weight / 5))
+	print("day_4_ago_score: {}".format(day_4_ago_score))
 
 	score = today_score + day_1_ago_score + day_2_ago_score + day_3_ago_score + day_4_ago_score
-	return score / get_possible_score(day)
+	print("score: {}".format(score))
+	return float(score) / float(get_possible_score(day))
+
+for i in range(8, 15):
+	day = Day.query.filter_by(date=datetime(2018, 4, i)).first()
+	print(get_days_progress(day))
+
