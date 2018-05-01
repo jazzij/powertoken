71c05aa0b7b4aa8b33cafe26614cef08c5bd18b1
Kesiel <a2zfranz@gmail.com>
2018-05-01 09:39:15 -0500

    Added db module.

---
+++ background/db.py
@@ -0,0 +1,14 @@
+"""
+Database module common to the background scripts and their various helpers.\n
+Created by Abigail Franz on 4/30/2018.\n
+"""
+
+from models import DB_PATH
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Set up the SQLAlchemy engine and connect it to the Sqlite database.
+engine = create_engine("sqlite:///" + DB_PATH)
+Base.metadata.bind = engine
+DbSession = sessionmaker(bind=engine)
+session = DbSession()
