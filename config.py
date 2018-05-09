c2d56a1eac77b6a883754841793d513ec1bee0b5
Kesiel <a2zfranz@gmail.com>
2018-05-09 13:08:15 -0500

    Modified code so database still tracks expired activities.

--- config.py
+++ config.py
@@ -8,20 +8,13 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Retrieve the CSRF token from a file on the server. Make sure this file isn't
-# world-readable! You can generate a good token with os.urandom(12) and then
-# copy-paste it into the file called "csrf".
-skey = ""
-with open("csrf") as fp:
-	skey = fp.read().strip()
-
 # The environment variables are currently set in the run_powertoken.sh script.
 # TODO: Add administrator email addresses to the ADMINS field.
 class Config(object):
 	"""
 	Name-value pairs for the Flask app configuration.
 	"""
-	SECRET_KEY = os.environ.get("SECRET_KEY") or skey
+	SECRET_KEY = os.environ.get("SECRET_KEY")
 	SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
 		"sqlite:///" + os.path.join(basedir, "data/pt-fade.db")
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
